7fbf512023b69903ee99ca44ea0ec93f79005260
João Álvaro <joaosilvaabc1@gmail.com>
2025-10-23 17:45:01 -0300

    feat(SPACE-178): conteúdo placeholder da trilha, navegação de itens

--- EntrenovaFlix/src/paginas/telaTrilhas.tsx
+++ EntrenovaFlix/src/paginas/telaTrilhas.tsx
@@ -2,6 +2,7 @@ import { BiSolidCameraMovie } from "react-icons/bi";
 import { GiBrain } from "react-icons/gi";
 import { IoPlay } from "react-icons/io5";
 import { PiMicrophoneFill } from "react-icons/pi";
+import { FaRegClock } from "react-icons/fa";
 import { MdKeyboardDoubleArrowLeft, MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
@@ -26,7 +27,7 @@ export default function TelaTrilhas() {
         <h2 className="sidebarTitulo">Como ampliar suas vendas online?</h2>
         <h2 className="sidebarSubtitulo">Materiais</h2>
 
-        <button className="sidebar-btn" onClick={() => navigate("/trilhas")}>
+        <button className="sidebar-btn">
           <BiSolidCameraMovie className="sidebar-icon" size={24} /> 
           Filme
         </button>
@@ -47,8 +48,27 @@ export default function TelaTrilhas() {
         </button>
       </div>
 
-      <div className="tituloTrilha">
-        <p>O PODER DO MARKETING PARA VENDAS</p>
+      <div className="conteudoTrilha">
+        <div>
+          <h1 className="tituloTrilha">O PODER DO MARKETING PARA VENDAS</h1>
+          <h3 className="subtituloTrilha">
+            <FaRegClock size={20} />
+            27 minutos
+          </h3>
+        </div>
+
+        <iframe width="900" height="500" src="https://www.youtube.com/embed/NpEaa2P7qZI?si=WZTsU1VWrqxcYf_G" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        <div className="navItems">
+            <button className="navBtn">
+              <MdKeyboardDoubleArrowLeft size={24} />
+              Atividade Anterior
+            </button>
+            <button className="navBtn">
+              Próxima atividade
+              <MdKeyboardDoubleArrowRight size={24} />
+            </button>
+        </div>
       </div>
     </div>
   );
