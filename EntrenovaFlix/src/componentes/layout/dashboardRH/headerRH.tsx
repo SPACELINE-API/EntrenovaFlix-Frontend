d765c5bd1c3512a294997e55d24c4e561d68028c
Leti-10 <leti.oli.lopes@gmail.com>
2025-11-19 20:03:31 -0300

    fix: Corrigido layout dashboards

--- EntrenovaFlix/src/componentes/layout/dashboardRH/headerRH.tsx
+++ EntrenovaFlix/src/componentes/layout/dashboardRH/headerRH.tsx
@@ -1,18 +1,25 @@
-import '../../../styles/headerRH.css';
+import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import "../../../styles/headerRH.css";
 
-interface HeaderRHProps {
-    userName?: string;
-    userAvatar?: string;
-    pageTitle?: string;
+interface HeaderRHProps { 
+    userName?: string; 
+    userAvatar?: string; 
+    pageTitle?: string; 
 }
 
 function HeaderRH({ userName = "PLACEHOLDER", userAvatar, pageTitle }: HeaderRHProps) {
+    const [open, setOpen] = useState(false);
+
     return (
         <header className="header-RH">
             <div className="header-RH-content">
                 <h2 className="header-RH-title">{pageTitle}</h2>
-
-                <div className="header-RH-user">
+                <div 
+                    className="user-menu" 
+                    onClick={() => setOpen(!open)}
+                >
                     <div className="user-avatar">
                         {userAvatar ? (
                             <img src={userAvatar} alt={userName} />
@@ -22,9 +29,18 @@ function HeaderRH({ userName = "PLACEHOLDER", userAvatar, pageTitle }: HeaderRHP
                             </div>
                         )}
                     </div>
-                    <div className="user-info">
-                        <span className="user-display-name">{userName.toUpperCase()}</span>
-                    </div>
+
+                    <span className="user-display-name">
+                        {userName.toUpperCase()}
+                    </span>
+                    <span className={`arrow ${open ? "open" : ""}`}><IoIosArrowDown/></span>
+                    {open && (
+                        <div className="dropdown-menu">
+                            <button onClick={() => console.log("logout")}>
+                                <FaArrowRightFromBracket/>  Logout
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         </header>
